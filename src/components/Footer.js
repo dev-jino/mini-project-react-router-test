f65a2a38031874cbd5c351579bef25eb4fcb464f
dev-jino <wlsgh2029@naver.com>
2023-10-10 10:13:47 +0900

    test2 - footer & main list change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,27 +2,23 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div className="e42_1369">
-          <div className="ei42_1369_22_50">
-            <div className="ei42_1369_22_51">
-              <div className="ei42_1369_22_52">
-                <div className="ei42_1369_22_53">
-                  <div className="ei42_1369_22_61">
-                    <span className="ei42_1369_22_61_18_93">HowMuch</span>
-                    <div className="ei42_1369_22_61_18_180"></div>
-                  </div>
-                  <span className="ei42_1369_22_64">
-                    현재 웹 사이트는 Mini-Project의 일환으로 실제로 물건을 판매하지 않습니다.
-                  </span>
+        <div className="footer">
+          <div className="footer-container">
+            <div className="footer-container-row">
+              <div className="footer-container-col-left">
+                <div className="logo-icon">
+                  <div className="logo-img"></div>
+                  <span className="logo-text">HowMuch</span>
                 </div>
+                <span className="footer-container-col-left-text">
+                  현재 웹 사이트는 Mini-Project의 일환으로 <br/>실제로 물건을 판매하지 않습니다.
+                </span>
               </div>
-              <div className="ei42_1369_22_70">
-                <div className="ei42_1369_22_71">
-                  <div className="ei42_1369_22_77"></div>
-                  <span className="ei42_1369_22_72">
-                    현재 웹사이트의 소스 코드는 하단의 Git Hub에서 확인하실 수 있습니다.
-                  </span>
-                </div>
+              <div className="footer-container-col-right">
+                <div className="gitbub-icon"></div>
+                <span className="footer-container-col-right-text">
+                  현재 웹사이트의 소스 코드는 <br/>하단의 Git Hub에서 확인하실 수 있습니다.
+                </span>
               </div>
             </div>
           </div>
